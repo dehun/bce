eb8ab55438a8c314cf0e71c63c0126437d965517
Yuriy Netesov <ynetesov@cogniance.com>
2017-05-26 18:35:35 +0200

    +++

--- src/BlockChain.hs
+++ src/BlockChain.hs
@@ -3,13 +3,6 @@ module BlockChain where
 import Data.Maybe
 import qualified Data.List as DList
 
-elemAt :: [a] -> Int -> Maybe a
-elemAt xs idx = continue 0 xs idx
-    where continue cidx (x:xs) idx
-              | cidx == idx = Just x
-              | otherwise = continue (cidx + 1) xs idx
-          continue _ [] _ = Nothing
-
 type Hash = String
 type PubKey = String    
 
@@ -17,22 +10,12 @@ type PubKey = String
 type TimeStamp = Int
 type WalletAddress = String    
 
-data BlockHeader = BlockHeader {
-      bhTransactionsHash :: Hash
-    , bhPrevBlockHeaderHash :: Hash
-    , bhNonce :: Int
-    , bhWallClockTime :: TimeStamp
-    } deriving (Show, Eq)
-
-
 data TxOutput = TxOutput { outputAmount :: Double
-                         , outputDestination :: WalletAddress
                          , outputPubKey :: PubKey
                          } deriving (Show, Eq)
 data TxOutputRef = TxOutputRef {
-      outputRefBlockIdx :: Int
-    , outputRefTxIdx :: Int
-    , outputRefIdx :: Int
+      outputRefTxId :: Hash
+    , outputRefOutputIdx :: Int
       } deriving (Show, Eq)
 
 
@@ -46,6 +29,30 @@ data Transaction =
         txInputs :: [TxInput]
       , txOutputs :: [TxOutput] } deriving (Show, Eq)
 
+data BlockHeader = BlockHeader {
+      bhTransactionsHash :: Hash
+    , bhPrevBlockHeaderHash :: Hash
+    , bhNonce :: Int
+    , bhWallClockTime :: TimeStamp
+    } deriving (Show, Eq)
+
+data Block = Block { blockHeader:: BlockHeader
+                   , blockTransactions :: [Transaction] } deriving (Show, Eq)
+
+data BlockChain = BlockChain { blockChainBlocks :: [Block] } deriving (Show, Eq)
+
+-- ^^^ data structures goes up here ^^^
+-- vvv experimental shit goes here  vvv
+
+elemAt :: [a] -> Int -> Maybe a
+elemAt xs idx = continue 0 xs idx
+    where continue cidx (x:xs) idx
+              | cidx == idx = Just x
+              | otherwise = continue (cidx + 1) xs idx
+          continue _ [] _ = Nothing
+
+                
+
 resolveTransaction :: Block -> Int -> Maybe Transaction
 resolveTransaction b idx = elemAt (blockTransactions b) idx
 
@@ -56,11 +63,14 @@ resolveInput (Transaction ins outs) idx = elemAt ins idx
 resolveOutput :: Transaction -> Int -> Maybe TxOutput
 resolveOutput tx idx = elemAt (txOutputs tx) idx
 
-resolveOutputInChain :: BlockChain -> TxOutputRef -> Maybe TxOutput
-resolveOutputInChain bc (TxOutputRef blockIdx txIdx outputIdx) = do
-      block <- resolveBlock bc blockIdx
-      tx <- resolveTransaction block txIdx
-      resolveOutput tx outputIdx
+-- resolveOutputInChain :: BlockChain -> TxOutputRef -> Maybe TxOutput
+-- resolveOutputInChain bc (TxOutputRef blockIdx txIdx outputIdx) = do
+--       block <- resolveBlock bc blockIdx
+--       tx <- resolveTransaction block txIdx
+--       resolveOutput tx outputIdx
+
+resolveBlock :: BlockChain -> Int -> Maybe Block                
+resolveBlock bc blockIndex = elemAt (blockChainBlocks bc) blockIndex
 
 verifyTransaction :: BlockChain -> Transaction -> Bool
 verifyTransaction bc tx =
@@ -69,14 +79,7 @@ verifyTransaction bc tx =
             where ref = inputOutputRef i
         resolvedInputs = map resolveInput $ txInputs tx
     in undefined
-
-data Block = Block { blockHeader:: BlockHeader
-                   , blockTransactions :: [Transaction] } deriving (Show, Eq)
-
-data BlockChain = BlockChain { blockChainBlocks :: [Block] } deriving (Show, Eq)
-
-resolveBlock :: BlockChain -> Int -> Maybe Block                
-resolveBlock bc blockIndex = elemAt (blockChainBlocks bc) blockIndex
+                
 
 verifyBlock :: Block -> Bool
 verifyBlock = undefined
